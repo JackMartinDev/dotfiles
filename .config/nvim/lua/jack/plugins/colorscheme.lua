985b7db065df09dbe6ba18913a04516c7590a42b
Jack Martin <jackmartin.dev@gmail.com>
2024-09-14 12:08:34 +0900

    Add catppuccin color scheme

--- .config/nvim/lua/jack/plugins/colorscheme.lua
+++ .config/nvim/lua/jack/plugins/colorscheme.lua
@@ -1,12 +1,22 @@
 return {
-	"navarasu/onedark.nvim",
+	"catppuccin/nvim",
+	name = "catppuccin",
 	priority = 1000,
 	config = function()
-		-- Theme inspired by Atom
-		require("onedark").setup({
-			style = "cool",
+		require("catppuccin").setup({
+			flavour = "macchiato",
 		})
-		-- load the colorscheme here
-		vim.cmd([[colorscheme onedark]])
+		vim.cmd([[colorscheme catppuccin]])
 	end,
 }
+
+-- return {
+-- 	"navarasu/onedark.nvim",
+-- 	priority = 1000,
+-- 	config = function()
+-- 		require("onedark").setup({
+-- 			style = "cool",
+-- 		})
+-- 		vim.cmd([[colorscheme onedark]])
+-- 	end,
+-- }
